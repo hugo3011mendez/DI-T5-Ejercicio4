8ddcc26f80cf848993818b3e63031244ed68065d
Hugo <hugo3011mendez@gmail.com>
2021-02-24 12:45:48 +0100

    Agregar archivos de proyecto.

---
+++ DibujoAhorcado/DibujoAhorcado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DibujoAhorcado
+{
+    public partial class DibujoAhorcado : UserControl
+    {
+
+        int errores;
+        [Category("Errores")]
+        [Description("Indica el número de errores cometidos")]
+        public int Errores
+        {
+            set
+            {
+                errores = value;
+            }
+
+            get
+            {
+                return errores;
+            }
+        }
+
+
+        public DibujoAhorcado()
+        {
+            InitializeComponent();
+        }
+    }
+}
